e45b97632f8de293aa36f11a9de517040645b536
Harsh-sd <harshsandilya05042002@gmail.com>
2024-03-22 00:09:43 +0530

    push method become complex

---
+++ stackusingqueue.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+class stack
+{
+    int n;
+    queue<int> q1;
+    queue<int> q2;
+
+public:
+    stack()
+    {
+        n = 0;
+    }
+    void push(int x)
+    {
+
+        q2.push(x);
+        n++;
+        while (!q1.empty())
+        {
+            q2.push(q1.front());
+            q1.pop();
+        }
+        queue<int> temp = q1;
+        q1 = q2;
+        q2 = temp;
+    }
+    void pop()
+    {
+        q1.pop();
+        n--;
+    }
+    int top()
+    {
+        return q1.front();
+    }
+};
+int main()
+{
+    stack s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+    cout << s.top();
+}
